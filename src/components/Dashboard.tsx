ce387bc2ba1d03cf1bc952e2b5c9ae65489a88d0
Maria Paula Mejía <mapis4045@gmail.com>
2025-10-16 18:55:23 -0500

    feat: Add Dashboard to organize all main page components

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Feed from "./Feed";
+
+
+export default function Dashboard() {
+  return (
+    <div className="bg-[#1e1c24] min-h-screen flex justify-center items-start p-6">
+      <Feed />
+    </div>
+  );
+}
