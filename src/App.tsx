4a74076e3a187f33f554a088325fc98f3ddf70b0
jnfep <1105366588@u.icesi.edu.co>
2025-10-16 19:18:01 -0500

    Added responsive sidebar with logo and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Sidebar from "./components/sidebar/sidebar"; // import your menu
+import "./App.css"; // import the layout styles
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-pink-500 hover:text-blue-400">
-      Hello tailwind, this is a test
-    </h1>
+    <div className="app-container">
+      {/* sidebar */}
+      <Sidebar />
+
+      {/* main content */}
+      <main className="main-content">
+      </main>
+    </div>
   );
 }
